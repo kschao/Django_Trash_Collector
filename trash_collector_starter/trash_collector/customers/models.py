d7de41a1101851c779e3aa87be3fd76b9d3fe329
Paul-Longo <imanazarite88@gmail.com>
2021-08-02 15:40:06 -0400

    updated the models.py customer class in trash_collector app folder

--- trash_collector_starter/trash_collector/customers/models.py
+++ trash_collector_starter/trash_collector/customers/models.py
@@ -6,4 +6,12 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey('accounts.User', blank=True,
+                             null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=25)
+    weekly_pickup_day = models.CharField(max_length=50)
+    one_time_pickup = models.DateField(null=True)
+    balance = models.IntegerField(default=0)
+    suspend_start = models.DateField(null=True)
+    suspend_finish = models.DateField(null=True)
