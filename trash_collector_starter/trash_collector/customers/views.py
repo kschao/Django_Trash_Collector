3b802e10571c8d947c0c1a8626833e52f4539f94
Paul-Longo <imanazarite88@gmail.com>
2021-08-03 11:11:15 -0400

    updated index method and created a method for create

--- trash_collector_starter/trash_collector/customers/views.py
+++ trash_collector_starter/trash_collector/customers/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Customer
 # Create your views here.
 
@@ -13,11 +15,28 @@ def index(request):
     try:
         # This line inside the 'try' will return the customer record of the logged-in user if one exists
         logged_in_customer = Customer.objects.get(user=user)
+        context = {
+            'logged_in_customer': logged_in_customer
+        }
     except:
         # TODO: Redirect the user to a 'create' function to finish the registration process if no customer record found
-        pass
+       return HttpResponseRedirect(reverse('customers:register'))
 
     # It will be necessary while creating a Customer/Employee to assign request.user as the user foreign key
 
     print(user)
-    return render(request, 'customers/index.html')
+    return render(request, 'customers/index.html', context)
+
+# User info registration
+def create(request):
+    if request.method == "POST":
+        user = request.user
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        zip_code = request.POST.get('zip_code')
+        weekly_pickup_day = request.POST.get('weekly_pickup_day')
+        new_customer = Customer(user=user, name=name, address=address, zip_code=zip_code, weekly_pickup_day=weekly_pickup_day)
+        new_customer.save()
+        return HttpResponseRedirect (reverse('customer:details'))
+    else:
+        return render(request, 'customer/create.html')
