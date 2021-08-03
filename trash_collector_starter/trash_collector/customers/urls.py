f0cbba94d882966563f3b74f5ee3214fb8e4fc20
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-03 11:06:33 -0500

    add path in customer urls.py

--- trash_collector_starter/trash_collector/customers/urls.py
+++ trash_collector_starter/trash_collector/customers/urls.py
@@ -7,4 +7,9 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
+    path('new/', views.create, name='register'),
+    path('weekly_pickup_day', views.weekly_pickup_day, name='weekly_pickup_day'),
+    path('one_time_pickup', views.one_time_pickup, name='one_time_pickup'),
+    path('suspend_start', views.suspend_start, name='suspend_start'),
+    path('balance', views.balance, name='balance')
 ]
