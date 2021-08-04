1251d3c236e7b3f5846255f9a12162f35841d223
Paul-Longo <imanazarite88@gmail.com>
2021-08-04 15:40:34 -0400

    updated path content in weekly pickup day and suspend start

--- trash_collector_starter/trash_collector/customers/urls.py
+++ trash_collector_starter/trash_collector/customers/urls.py
@@ -8,8 +8,8 @@ app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
     path('new/', views.create, name='register'),
-    path('weekly_pickup_day', views.weekly_pickup_day, name='weekly_pickup_day'),
+    path('weekly_pickup_day', views.create, name='weekly_pickup_day'),
     path('one_time_pickup', views.one_time_pickup, name='one_time_pickup'),
-    path('suspend_start', views.suspend_start, name='suspend_start'),
+    path('suspend_start', views.suspension_request, name='suspend_start'),
     path('balance', views.balance, name='balance')
 ]
